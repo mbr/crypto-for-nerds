b8cfd3bbfc2dd1bb3121d35b452ef9e3383822de
Marc Brinkmann <git@marcbrinkmann.de>
2014-02-06 11:59:55 +0100

    Use better theme.

--- conf.py
+++ conf.py
@@ -100,6 +100,10 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+from better import better_theme_path
+html_theme_path = [better_theme_path]
+html_theme = 'better'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
